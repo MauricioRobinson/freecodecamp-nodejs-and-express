3ae31348e45a0de5b8a6a281006ba747cb98f6a0
Mauricio Robinson <marsjr97@gmail.com>
2023-09-19 16:27:27 -0400

    [UPDATE]: Adding the .env file

--- myApp.js
+++ myApp.js
@@ -1,40 +1,21 @@
+require('dotenv').config();
 let express = require('express');
-console.log("Hello World")
+console.log('Hello World');
 let app = express();
 
+app.use('/public', express.static(__dirname + '/public'));
 
+app.get('/', function (req, res) {
+  // res.send("Hello Express")
+  res.sendFile(__dirname + '/views/index.html');
+});
 
+app.get('/json', function (req, res) {
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    res.json({ message: 'HELLO JSON' });
+  } else {
+    res.json({ message: 'Hello json' });
+  }
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
